da552faba3a60273f7b0466ef8b3e58eb5ce5420
talia-protasova <talia.coruja@gmail.com>
2026-04-20 18:41:17 +0200

    fix: remove duplicate BrowserRouter from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './src/pages/home/HomePage.tsx';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+        </Routes>
     );
 }
 
